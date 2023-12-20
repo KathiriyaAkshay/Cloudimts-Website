d3b811460be91fd79b13fc98a4e17907eb29f024
kishan maheta <mahetakishan01@example.com>
2023-12-20 10:19:03 +0530

    added download page feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import BasicLayout from "./components/Header";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Login from "./pages/Login";
+
 import { routes } from "./routes/routes";
 import BasicContext from "./hooks";
 import NotFound from "./components/NotFound";
 import ReportSummary from "./components/ReportSummary";
+import Downloads from "./pages/Downloads/Downloads";
 
 function App() {
   return (
@@ -15,7 +17,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/admin/login" element={<Login />} />
         <Route path="/not-found" element={<NotFound />} />
-        <Route path="/patientreport/:id" element={<ReportSummary />} />     
+        <Route path="/patientreport/:id" element={<ReportSummary />} />
+        <Route path="/downloads" element={<Downloads/>} />     
         <Route path="*" element={<Navigate to="/not-found" />} />
         {routes?.map(({ path, component: Component }, index) => (
           <Route
