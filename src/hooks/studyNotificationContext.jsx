e193432e4b550689109c377aa00400742d31866c
keyur-v-crest <keyur.vaghasiya@crestinfosystems.com>
2024-02-17 15:55:44 +0530

    Update assign user tooltip update to live

--- src/hooks/studyNotificationContext.jsx
+++ src/hooks/studyNotificationContext.jsx
@@ -94,7 +94,7 @@ const StudyNotificationProvider = ({ children }) => {
                 return {...element, status: "Assigned", 
                   updated_at: eventData.payload.data.updated_at, 
                   study_description: eventData.payload.data.study_description, 
-                  assign_user: "KeyurVAghasiya"
+                  assign_user: eventData.payload.data.assign_user
                 } ;
                  
               } else{
